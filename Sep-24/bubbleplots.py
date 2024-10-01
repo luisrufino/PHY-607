57703ed785fd5717659693019d1fac428ca7d569
jedcomp607 <jetulu@syr.edu>
2024-10-01 17:43:34 -0400

    bubble sorts plot

--- Sep-24/bubbleplots.py
+++ Sep-24/bubbleplots.py
@@ -1,1 +1,30 @@
-plots for bubble sort algorithm
+from sort_alg import bubble
+import numpy as np
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sizes = []
+times = []
+for i in range (1,11):
+    size =  i*i*50
+    randomlist = np.random.randint(1, 1000, size=size)
+    start = time.time()
+    sorted = bubble(randomlist)
+    elapsedtime = (time.time() - start)
+    sizes.append(size)
+    times.append(elapsedtime)
+    print("The time it took to do bubble-sort of randomly sorted",size, "integers is",elapsedtime )
+
+print("Times array",times)
+print("Sizes array",sizes)
+
+values = {'n': sizes, 'Time results': times}
+df = pd.DataFrame(values)
+print(df)
+
+plot = df.plot(x='n', y='Time results', kind='line')
+plot.set_xlabel("size of array")
+plot.set_ylabel("time elapsed")
+plot.set_title('size vs time elapsed')
+plt.show()
