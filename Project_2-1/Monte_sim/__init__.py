533e9297ce7a46ea8aa6f2a88386019a0afdf3a2
Luis Rufino <60110076+luisrufino@users.noreply.github.com>
2024-10-22 18:34:41 -0400

    Delete Project_2-1 directory

--- Project_2-1/Monte_sim/__init__.py
+++
@@ -1,6 +0,0 @@
-# /Users/luisrufino/PycharmProjects/CompPhysics/PHY-607/Project_2-1/Monte_sim/__init__.py
-
-from .box import Box
-from .particle import Particle
-from .mc_sim import run_simulation
-from.utils import rejection_sampling, inverse_cdf_sampling
